069e2852dd5f22d56d1413b9fc96bebedb10b981
codechiefVignesh <vignesharavindh72@gmail.com>
2025-07-08 22:17:55 +0530

    home page backend + wardrobe button + ar commit

--- main/homepage/admin.py
+++ main/homepage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, WardrobeItem
 
 admin.site.register(Category)
-admin.site.register(Product) 
+admin.site.register(Product)
+admin.site.register(WardrobeItem) 
--- main/homepage/models.py
+++ main/homepage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -20,6 +21,25 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
 
 
+class WardrobeItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    added_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user','category')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.category.name}"
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_body_image = models.ImageField(upload_to='user_bodies/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=255)
@@ -37,3 +57,5 @@ class Product(models.Model):
 
     class Meta:
         verbose_name_plural = "Products"
+
+
